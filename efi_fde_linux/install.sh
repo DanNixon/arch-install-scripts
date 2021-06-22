66bb2f4bb75e0625834e978a2058aa2eabc5274f
Dan Nixon <dan@dan-nixon.com>
2021-06-22 08:08:26 +0100

    Move LUKS to script

--- efi_fde_linux/install.sh
+++ efi_fde_linux/install.sh
@@ -12,17 +12,7 @@ disk_prepare_efi "$disk"
 efi_part="${disk}1"
 root_part="${disk}2"
 
-
-cryptsetup \
-  --cipher aes-xts-plain64 \
-  --hash sha512 \
-  --use-random \
-  --verify-passphrase \
-  luksFormat \
-  "$root_part"
-cryptsetup luksOpen "$root_part" root
-
-
+luks_create "$root_part" root
 rootfs_create '/dev/mapper/root'
 efifs_mount "$efi_part"
 
